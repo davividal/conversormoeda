66bed6ceac7fd185e1b7ac369da84ec439445e03
Davi Koscianski Vidal <davividal@gmail.com>
2016-11-18 20:06:02 -0200

    Prova 4º bimestre

--- app/src/main/java/br/com/davividal/conversormoeda/MainActivity.java
+++ app/src/main/java/br/com/davividal/conversormoeda/MainActivity.java
@@ -5,12 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioGroup;
-import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Response.Listener<String>, Response.ErrorListener {
     private static final Float ExangeRate = (float) 3.5;
+    private static final String ServiceUrl = "http://ec2-52-67-202-68.sa-east-1.compute.amazonaws.com/conversao.php?moeda=%s&valor=%f";
+    private String code = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,25 +28,43 @@ public class MainActivity extends AppCompatActivity {
 
     public void convert(View view) {
         EditText originalValueText = (EditText) findViewById(R.id.originalValue);
-        TextView newValueText = (TextView) findViewById(R.id.newValue);
         RadioGroup currency = (RadioGroup) findViewById(R.id.currency);
-        Float originalValue, newValue = null;
-        String code = "";
+        Float originalValue;
 
-        assert null != currency && null != originalValueText && null != newValueText;
+        String currencyName = "";
+
+        assert null != currency && null != originalValueText;
 
         originalValue = Float.parseFloat(String.valueOf(originalValueText.getText()));
 
         if (R.id.dollar == currency.getCheckedRadioButtonId()) {
-            newValue = originalValue * ExangeRate;
+            currencyName = "dolar";
             code = "US$";
-        } else if (R.id.real == currency.getCheckedRadioButtonId()) {
-            newValue = originalValue / ExangeRate;
-            code = "R$";
+        } else if (R.id.euro == currency.getCheckedRadioButtonId()) {
+            currencyName = "euro";
+            code = "€";
         }
 
-        assert null != newValue;
+        String url = String.format(new Locale("en", "us"), ServiceUrl, currencyName, originalValue);
+        RequestQueue queue = CustomVolleyRequestQueue.getInstance(getApplicationContext()).getRequestQueue();
 
-        newValueText.setText(String.format(new Locale("pt", "br"), "%s %.2f", code, newValue));
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, this, this);
+        queue.add(stringRequest);
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        Toast.makeText(this, "Ocorreu um erro!", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onResponse(String response) {
+        Float newValue = Float.parseFloat(response);
+
+        Toast.makeText(
+                this,
+                String.format(new Locale("pt", "br"), "Novo valor: %s %.2f", code, newValue),
+                Toast.LENGTH_LONG
+        ).show();
     }
 }
